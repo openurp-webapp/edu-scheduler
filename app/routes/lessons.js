082c6031ac63ac023413ffcf3d62d06135b3f2bc
chaostone <duantihua@gmail.com>
2015-03-05 16:56:37 +0800

    add lesson.hbs

--- app/routes/lessons.js
+++ app/routes/lessons.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function(params) {
+        return { items:[{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}]}
+    }
 });
